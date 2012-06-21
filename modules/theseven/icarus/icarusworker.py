TheSeven/Modular-Python-Bitcoin-Miner

commit 4a8cbaf40a64c093384994bb3d0a2dae845c4deb

SQLiteStats: Implement periodic statistics capturing and fix lots of bugs

modules/theseven/icarus/icarusworker.py
@@ -70,7 +70,7 @@ class IcarusWorker(BaseWorker):
     if not "jobinterval" in self.settings or not self.settings.jobinterval: self.settings.jobinterval = 60
     # We can't change the port name or baud rate on the fly, so trigger a restart if they changed.
     # self.port/self.baudrate are cached copys of self.settings.port/self.settings.baudrate
-    if self.settings.port != self.port or self.settings.baudrate != self.baudrate: self.async_restart()
+    if self.started and self.settings.port != self.port or self.settings.baudrate != self.baudrate: self.async_restart()
     
 
   # Reset our state. Called both from the constructor and from self.start().

modules/theseven/simplers232/simplers232worker.py
@@ -70,7 +70,7 @@ class SimpleRS232Worker(BaseWorker):
     if not "jobinterval" in self.settings or not self.settings.jobinterval: self.settings.jobinterval = 60
     # We can't change the port name or baud rate on the fly, so trigger a restart if they changed.
     # self.port/self.baudrate are cached copys of self.settings.port/self.settings.baudrate
-    if self.settings.port != self.port or self.settings.baudrate != self.baudrate: self.async_restart()
+    if self.started and self.settings.port != self.port or self.settings.baudrate != self.baudrate: self.async_restart()
     
 
   # Reset our state. Called both from the constructor and from self.start().

core/job.py
@@ -105,7 +105,7 @@ class Job(object):
       with self.worker.stats.lock: self.worker.stats.sharesinvalid += 1
       self.core.event(300, self.worker, "nonceinvalid", nonceval, None, self.worker, self.worksource, self.blockchain, self)
       return False
-    self.core.log(self.worker.settings.name, "Found share: %s:%s:%s\n" % (self.worksource.settings.name, hexlify(self.data[:76]).decode("ascii"), hexlify(nonce).decode("ascii")), 350, "g")
+    self.core.log(self.worker, "Found share: %s:%s:%s\n" % (self.worksource.settings.name, hexlify(self.data[:76]).decode("ascii"), hexlify(nonce).decode("ascii")), 350, "g")
     noncediff = 65535. * 2**48 / struct.unpack("<Q", hash[-12:-4])[0]
     self.core.event(450, self.worker, "noncevalid", nonceval, str(noncediff), self.worker, self.worksource, self.blockchain, self)
     if hash[::-1] > self.target[::-1]:
@@ -117,6 +117,7 @@ class Job(object):
     
     
   def nonce_handled_callback(self, nonce, noncediff, result):
+    nonceval = struct.unpack("<I", nonce)[0]
     if result == True:
       self.core.log(self.worker, "%s accepted share %s (difficulty %.5f)\n" % (self.worksource.settings.name, hexlify(nonce).decode("ascii"), noncediff), 250, "gB")
       with self.worker.stats.lock: self.worker.stats.sharesaccepted += self.difficulty

modules/fpgamining/x6500/x6500hotplug.py
@@ -182,7 +182,7 @@ class X6500HotplugWorker(BaseWorker):
     if not "scaninterval" in self.settings or not self.settings.scaninterval: self.settings.scaninterval = 10
     # We can't switch the driver on the fly, so trigger a restart if it changed.
     # self.useftd2xx is a cached copy of self.settings.useftd2xx
-    if self.settings.useftd2xx != self.useftd2xx: self.async_restart()
+    if self.started and self.settings.useftd2xx != self.useftd2xx: self.async_restart()
     # Push our settings down to our children
     fields = ["takeover", "uploadfirmware", "firmware", "initialspeed", "maximumspeed", "tempwarning", "tempcritical",
               "invalidwarning", "invalidcritical", "speedupthreshold", "jobinterval", "pollinterval"]

modules/fpgamining/x6500/x6500worker.py
@@ -125,7 +125,8 @@ class X6500Worker(BaseWorker):
     if not "pollinterval" in self.settings or not self.settings.pollinterval: self.settings.pollinterval = 0.1
     # We can't switch the device or driver on the fly, so trigger a restart if they changed.
     # self.serial/self.useftd2xx are cached copys of self.settings.serial/self.settings.useftd2xx
-    if self.settings.serial != self.serial or self.settings.useftd2xx != self.useftd2xx: self.async_restart()
+    if self.started and (self.settings.serial != self.serial or self.settings.useftd2xx != self.useftd2xx):
+      self.async_restart()
     # We need to inform the proxy about a poll interval change
     if self.started and self.settings.pollinterval != self.pollinterval: self._notify_poll_interval_changed()
     for child in self.children: child.apply_settings()
@@ -283,8 +284,8 @@ class X6500Worker(BaseWorker):
     if self.children:
       self.children[0].stats.temperature = fpga0
       self.children[1].stats.temperature = fpga1
-      self.core.event(350, self.children[0], "temperature", fpga0 * 1000, "%f °C" % fpga0)
-      self.core.event(350, self.children[1], "temperature", fpga1 * 1000, "%f °C" % fpga1)
+      self.core.event(350, self.children[0], "temperature", fpga0 * 1000, "%f \xc2\xb0C" % fpga0)
+      self.core.event(350, self.children[1], "temperature", fpga1 * 1000, "%f \xc2\xb0C" % fpga1)
 
       
   def send_job(self, fpga, job):

modules/theseven/bflsingle/bflsingleworker.py
@@ -65,7 +65,7 @@ class BFLSingleWorker(BaseWorker):
     if not "port" in self.settings or not self.settings.port: self.settings.port = "/dev/ttyUSB0"
     # We can't change the port name on the fly, so trigger a restart if they changed.
     # self.port is a cached copy of self.settings.port.
-    if self.settings.port != self.port: self.async_restart()
+    if self.started and self.settings.port != self.port: self.async_restart()
     
 
   # Reset our state. Called both from the constructor and from self.start().
@@ -203,7 +203,7 @@ class BFLSingleWorker(BaseWorker):
           if response[:23] != b"Temperature (celcius): " or response[-1:] != b"\n":
             raise Exception("Bad ZLX response: %s\n" % response.decode("ascii", "replace").strip())
           self.stats.temperature = float(response[23:-1])
-          self.core.event(350, self.children[0], "temperature", self.stats.temperature * 1000, "%f °C" % self.stats.temperature)
+          self.core.event(350, self.children[0], "temperature", self.stats.temperature * 1000, "%f \xc2\xb0C" % self.stats.temperature)
           if self.shutdown: break
 
           # Wait while the device is processing the job. If the job gets canceled, we will be woken up.

modules/theseven/ftdijtag/ftdijtagworker.py
@@ -100,7 +100,7 @@ class FTDIJTAGWorker(BaseWorker):
     if not "pollinterval" in self.settings or not self.settings.pollinterval: self.settings.pollinterval = 0.1
     # We can't switch the device on the fly, so trigger a restart if it changed.
     # self.serial is a cached copys of self.settings.serial
-    if self.settings.serial != self.serial: self.async_restart()
+    if self.started and self.settings.serial != self.serial: self.async_restart()
     # We need to inform the proxy about a poll interval change
     if self.started and self.settings.pollinterval != self.pollinterval: self._notify_poll_interval_changed()
     for child in self.children: child.apply_settings()
@@ -256,7 +256,7 @@ class FTDIJTAGWorker(BaseWorker):
       for fpga in temperatures:
         if len(self.children) > fpga:
           self.children[fpga].stats.temperature = temperatures[fpga]
-          self.core.event(350, self.children[fpga], "temperature", temperatures[fpga] * 1000, "%f °C" % temperatures[fpga])
+          self.core.event(350, self.children[fpga], "temperature", temperatures[fpga] * 1000, "%f \xc2\xb0C" % temperatures[fpga])
 
       
   def send_job(self, fpga, job):

modules/theseven/sqlite/sqlitestats.py
@@ -28,9 +28,11 @@
 
 import os
 import time
+import numbers
 import sqlite3
-from threading import RLock
+from threading import RLock, Condition, Thread
 from core.basefrontend import BaseFrontend
+from core.statistics import Statistics
 
 
 
@@ -45,6 +47,7 @@ class SQLiteStats(BaseFrontend):
     "filename": {"title": "Database file name", "type": "string", "position": 1000},
     "loglevel": {"title": "Log level", "type": "int", "position": 2000},
     "eventlevel": {"title": "Event filter level", "type": "int", "position": 2100},
+    "statinterval": {"title": "Statistics logging interval", "type": "int", "position": 3000},
   })
 
 
@@ -52,6 +55,7 @@ class SQLiteStats(BaseFrontend):
     super(SQLiteStats, self).__init__(core, state)
     self.lock = RLock()
     self.conn = None
+    self.statwakeup = Condition()
 
 
   def apply_settings(self):
@@ -59,20 +63,35 @@ class SQLiteStats(BaseFrontend):
     if not "filename" in self.settings or not self.settings.filename: self.settings.filename = "stats.db"
     if not "loglevel" in self.settings: self.settings.loglevel = self.core.default_loglevel
     if not "eventlevel" in self.settings: self.settings.eventlevel = self.core.default_loglevel
-    if self.started and self.settings.filename != self.filename: self.async_restart()
+    if not "statinterval" in self.settings: self.settings.statinterval = 60
+    if not "worksourceinterval" in self.settings: self.settings.worksourceinterval = 60
+    if not "blockchaininterval" in self.settings: self.settings.blockchaininterval = 60
+    if self.started:
+      if self.settings.filename != self.filename: self.async_restart()
+      else:
+        with self.statwakeup: self.statwakeup.notify()
 
 
   def _start(self):
     super(SQLiteStats, self)._start()
     with self.lock:
+      self.shutdown = False
       self.filename = self.settings.filename
       self.db = sqlite3.connect(self.filename, check_same_thread = False)
+      self.db.text_factory = str
       self.cursor = self.db.cursor()
       self._check_schema()
       self.eventtypes = {}
+      self.statcolumns = {}
+      self.statthread = Thread(None, self._statloop, "%s_statthread" % self.settings.name)
+      self.statthread.daemon = True
+      self.statthread.start()
 
 
   def _stop(self):
+    self.shutdown = True
+    with self.statwakeup: self.statwakeup.notify()
+    self.statthread.join(5)
     with self.lock:
       self.cursor.close()
       self.cursor = None
@@ -112,7 +131,35 @@ class SQLiteStats(BaseFrontend):
                                              ":message, :worker, :worksource, :blockchain, :job)",
                           {"level": level, "timestamp": timestamp, "source": source, "type": eventtype, "argument": arg,
                            "message": message, "worker": worker, "worksource": worksource, "blockchain": blockchain, "job": job})
-    
+      
+      
+  def _statloop(self):
+    while not self.shutdown:
+      with self.statwakeup:
+        if self.settings.statinterval <= 0: self.statwakeup.wait()
+        else:
+          now = time.time()
+          stats = Statistics(obj = self.core, ghashes = self.core.stats.ghashes, starttime = self.core.stats.starttime)
+          stats.avgmhps =  1000. * stats.ghashes / (now - stats.starttime)
+          stats.children = self.core.get_worker_statistics() \
+                         + self.core.get_work_source_statistics() \
+                         + self.core.get_blockchain_statistics()
+          with self.lock:
+            self._insert_stats(now, stats)
+            self.db.commit()
+          self.statwakeup.wait(self.settings.statinterval)
+          
+          
+  def _insert_stats(self, timestamp, stats, parent = None):
+    self.cursor.execute("INSERT INTO [statrow]([timestamp], [subject], [parent]) VALUES(:timestamp, :subject, :parent)",
+                        {"timestamp": timestamp, "subject": self._get_object_id(stats.obj), "parent": parent})
+    row = self.cursor.lastrowid
+    for key, value in stats.items():
+      if isinstance(value, numbers.Number):
+        self.cursor.execute("INSERT INTO [statfield]([row], [column], [value]) VALUES(:row, :column, :value)",
+                            {"row": row, "column": self._get_statcolumn_id(key), "value": value})
+    for child in stats.children: self._insert_stats(timestamp, child, row)
+        
     
   def _get_objecttype_id(self, objtype):
     if hasattr(objtype, "_ext_theseven_sqlite_objtypeid"): return objtype._ext_theseven_sqlite_objtypeid
@@ -165,46 +212,73 @@ class SQLiteStats(BaseFrontend):
     return id
 
 
+  def _get_statcolumn_id(self, column):
+    if column in self.statcolumns: return self.statcolumns[column]
+    try:
+      self.cursor.execute("SELECT [id] FROM [statcolumn] WHERE [name] = :name", {"name": column})
+      id = int(self.cursor.fetchone()[0])
+    except:
+      self.cursor.execute("INSERT INTO [statcolumn]([name]) VALUES(:name)", {"name": column})
+      id = self.cursor.lastrowid
+    self.statcolumns[column] = id
+    return id
+
+
   def _check_schema(self):
     try:
       self.cursor.execute("SELECT [value] FROM [dbinfo] WHERE [key] = 'version'")
       version = int(self.cursor.fetchone()[0])
     except: version = 0
-    if version == 0:
-      self.cursor.execute("CREATE TABLE [dbinfo]([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
-                                                "[key] TEXT UNIQUE NOT NULL, "
-                                                "[value] TEXT UNIQUE NOT NULL)")
-      self.cursor.execute("CREATE TABLE [objecttype]([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
-                                       "             [name] TEXT UNIQUE NOT NULL)")
-      self.cursor.execute("CREATE TABLE [object]([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
-                                                "[type] INTEGER NOT NULL REFERENCES [objecttype] ON DELETE RESTRICT ON UPDATE RESTRICT, "
-                                                "[name] TEXT UNIQUE NOT NULL)")
-      self.cursor.execute("CREATE TABLE [job]([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
-                                             "[worksource] INTEGER NOT NULL REFERENCES [object] ON DELETE RESTRICT ON UPDATE RESTRICT, "
-                                             "[data] BLOB NOT NULL)")
-      self.cursor.execute("CREATE TABLE [eventtype]([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
-                                                   "[name] TEXT UNIQUE NOT NULL)")
-      self.cursor.execute("CREATE TABLE [event]([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+    try:
+      self.db.commit()
+      if version == 0:
+        self.cursor.execute("CREATE TABLE [dbinfo]([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+                                                  "[key] TEXT UNIQUE NOT NULL, "
+                                                  "[value] TEXT UNIQUE NOT NULL)")
+        self.cursor.execute("CREATE TABLE [objecttype]([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+                                         "             [name] TEXT UNIQUE NOT NULL)")
+        self.cursor.execute("CREATE TABLE [object]([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+                                                  "[type] INTEGER NOT NULL REFERENCES [objecttype] ON DELETE RESTRICT ON UPDATE RESTRICT, "
+                                                  "[name] TEXT NOT NULL, "
+                                                  "CONSTRAINT object_unique_type_name UNIQUE (type, name))")
+        self.cursor.execute("CREATE TABLE [job]([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+                                               "[worksource] INTEGER NOT NULL REFERENCES [object] ON DELETE RESTRICT ON UPDATE RESTRICT, "
+                                               "[data] BLOB NOT NULL)")
+        self.cursor.execute("CREATE TABLE [eventtype]([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+                                                     "[name] TEXT UNIQUE NOT NULL)")
+        self.cursor.execute("CREATE TABLE [event]([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+                                                 "[level] INTEGER NOT NULL, "
+                                                 "[timestamp] REAL NOT NULL, "
+                                                 "[source] INTEGER NOT NULL REFERENCES [object] ON DELETE RESTRICT ON UPDATE RESTRICT, "
+                                                 "[type] INTEGER NOT NULL REFERENCES [eventtype] ON DELETE RESTRICT ON UPDATE RESTRICT, "
+                                                 "[argument] INTEGER NULL, [message] TEXT NULL, "
+                                                 "[worker] INTEGER NULL REFERENCES [object] ON DELETE RESTRICT ON UPDATE RESTRICT, "
+                                                 "[worksource] INTEGER NULL REFERENCES [object] ON DELETE RESTRICT ON UPDATE RESTRICT, "
+                                                 "[blockchain] INTEGER NULL REFERENCES [object] ON DELETE RESTRICT ON UPDATE RESTRICT, "
+                                                 "[job] INTEGER NULL REFERENCES [job] ON DELETE RESTRICT ON UPDATE RESTRICT)")
+        self.cursor.execute("CREATE TABLE [log]([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
                                                "[level] INTEGER NOT NULL, "
                                                "[timestamp] REAL NOT NULL, "
-                                               "[source] INTEGER NOT NULL REFERENCES [object] ON DELETE RESTRICT ON UPDATE RESTRICT, "
-                                               "[type] INTEGER NOT NULL REFERENCES [eventtype] ON DELETE RESTRICT ON UPDATE RESTRICT, "
-                                               "[argument] INTEGER NULL, [message] TEXT NULL, "
-                                               "[worker] INTEGER NULL REFERENCES [object] ON DELETE RESTRICT ON UPDATE RESTRICT, "
-                                               "[worksource] INTEGER NULL REFERENCES [object] ON DELETE RESTRICT ON UPDATE RESTRICT, "
-                                               "[blockchain] INTEGER NULL REFERENCES [object] ON DELETE RESTRICT ON UPDATE RESTRICT, "
-                                               "[job] INTEGER NULL REFERENCES [job] ON DELETE RESTRICT ON UPDATE RESTRICT)")
-      self.cursor.execute("CREATE TABLE [log]([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
-                                             "[level] INTEGER NOT NULL, "
-                                             "[timestamp] REAL NOT NULL, "
-                                             "[source] INTEGER NOT NULL REFERENCES [object] ON DELETE RESTRICT ON UPDATE RESTRICT)")
-      self.cursor.execute("CREATE TABLE [logfragment]([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
-                                                     "[parent] INTEGER NOT NULL REFERENCES [log] ON DELETE RESTRICT ON UPDATE RESTRICT, "
-                                                     "[message] TEXT NOT NULL, "
-                                                     "[format] TEXT NOT NULL)")
-      self.cursor.execute("INSERT INTO [dbinfo]([key], [value]) VALUES('version', :version)", {"version": version + 1})
-      self.db.commit()
-#    if version == 1:
-#      self.cursor.execute("")
-#      self.cursor.execute("UPDATE [dbinfo] SET [value] = :version WHERE [key] = 'version'", {"version": version + 1})
-#      self.db.commit()
+                                               "[source] INTEGER NOT NULL REFERENCES [object] ON DELETE RESTRICT ON UPDATE RESTRICT)")
+        self.cursor.execute("CREATE TABLE [logfragment]([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+                                                       "[parent] INTEGER NOT NULL REFERENCES [log] ON DELETE RESTRICT ON UPDATE RESTRICT, "
+                                                       "[message] TEXT NOT NULL, "
+                                                       "[format] TEXT NOT NULL)")
+        self.cursor.execute("INSERT INTO [dbinfo]([key], [value]) VALUES('version', :version)", {"version": version + 1})
+        self.db.commit()
+        version = 1
+      if version == 1:
+        self.cursor.execute("CREATE TABLE [statcolumn]([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+                                                      "[name] TEXT UNIQUE NOT NULL)")
+        self.cursor.execute("CREATE TABLE [statrow]([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+                                                   "[timestamp] REAL NOT NULL, "
+                                                   "[subject] INTEGER NOT NULL REFERENCES [object] ON DELETE RESTRICT ON UPDATE RESTRICT, "
+                                                   "[parent] INTEGER NULL REFERENCES [statrow] ON DELETE RESTRICT ON UPDATE RESTRICT)")
+        self.cursor.execute("CREATE TABLE [statfield]([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+                                                     "[row] INTEGER NOT NULL REFERENCES [statrow] ON DELETE RESTRICT ON UPDATE RESTRICT, "
+                                                     "[column] INTEGER NOT NULL REFERENCES [statcolumn] ON DELETE RESTRICT ON UPDATE RESTRICT, "
+                                                     "[value] REAL NOT NULL)")
+        self.cursor.execute("UPDATE [dbinfo] SET [value] = :version WHERE [key] = 'version'", {"version": version + 1})
+        self.db.commit()
+        version = 2
+    except: self.db.rollback()

commit 278c4698b1fcc37f8afc2d283eafd6696ddcd39c

Fix AttributeError when creating new Icarus worker.
Prior to this commit, I was unable to add an Icarus worker via the
web interface.  I got an error dialog which said:

  AttributeError: 'IcarusWorker' object has no attribute 'baudrate'

This appears to be a regression introduced in commit 4a8cbaf4.  The
same bug is also present in SimpleRS23Worker, so I fixed it there
as well.

modules/theseven/icarus/icarusworker.py
@@ -70,7 +70,7 @@ class IcarusWorker(BaseWorker):
     if not "jobinterval" in self.settings or not self.settings.jobinterval: self.settings.jobinterval = 60
     # We can't change the port name or baud rate on the fly, so trigger a restart if they changed.
     # self.port/self.baudrate are cached copys of self.settings.port/self.settings.baudrate
-    if self.started and self.settings.port != self.port or self.settings.baudrate != self.baudrate: self.async_restart()
+    if self.started and (self.settings.port != self.port or self.settings.baudrate != self.baudrate): self.async_restart()
     
 
   # Reset our state. Called both from the constructor and from self.start().

modules/theseven/simplers232/simplers232worker.py
@@ -70,7 +70,7 @@ class SimpleRS232Worker(BaseWorker):
     if not "jobinterval" in self.settings or not self.settings.jobinterval: self.settings.jobinterval = 60
     # We can't change the port name or baud rate on the fly, so trigger a restart if they changed.
     # self.port/self.baudrate are cached copys of self.settings.port/self.settings.baudrate
-    if self.started and self.settings.port != self.port or self.settings.baudrate != self.baudrate: self.async_restart()
+    if self.started and (self.settings.port != self.port or self.settings.baudrate != self.baudrate): self.async_restart()
     
 
   # Reset our state. Called both from the constructor and from self.start().